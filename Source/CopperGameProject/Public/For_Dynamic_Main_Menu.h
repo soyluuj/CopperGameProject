a1602e773965cc37bdd58cce0bf6264487ae65f1
fenatu <144089770+fenatu@users.noreply.github.com>
2026-06-17 13:56:10 +0800

    intergrating VSC c++ codebase for custom classes
    simple additions, no edits to the project settings

---
+++ Source/CopperGameProject/Public/For_Dynamic_Main_Menu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "For_Dynamic_Main_Menu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COPPERGAMEPROJECT_API AFor_Dynamic_Main_Menu : public ACameraActor
+{
+	GENERATED_BODY()
+	
+};
